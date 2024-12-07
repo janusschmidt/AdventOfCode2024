50be8870a52d987293ac74cd52e24ac3d24f62ac
Janus Schmidt <janus.schmidt@velliv.com>
2024-12-07 13:32:41 +0100

    Day1 part 1 done.

---
+++ src/Day1/Day1/Part1.cs
@@ -0,0 +1,16 @@
+namespace Day1;
+
+public class Part1
+{
+    public static int GetTotal(int[] a1, int[] a2)
+    {
+        var arr1Sorted = a1.OrderBy(x => x).ToList();
+        var arr2Sorted = a2.OrderBy(x => x).ToList();
+
+        var zipped = arr1Sorted.Zip(arr2Sorted);
+
+        var totalDistance = zipped.Sum(x => Math.Abs(x.First - x.Second));
+        
+        return totalDistance;
+    }
+}
---
+++ src/Day1/Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day1;
+
+var projectDir = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName;
+var input = File.ReadAllLines(Path.Combine(projectDir, "input.txt"));
+
+var numbers = input.Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToList();
+var a1 = numbers.Select(x => int.Parse(x[0])).ToArray();
+var a2 = numbers.Select(x => int.Parse(x[1])).ToArray();
+
+Console.Write(Part1.GetTotal(a1, a2));
