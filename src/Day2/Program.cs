e6fccd1e705e456336a2bc1272e906d668d4179e
Janus Schmidt <janus.schmidt@velliv.com>
2024-12-07 19:32:41 +0100

    Day2 part 1 done

--- src/Day2/Program.cs
+++ src/Day2/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using InputParser;
 
-Console.WriteLine("Hello, World!");
+var parser = new Parser();
+var input = parser.GetIntArrayOfRowsAsArrays();
+
+var safeCount = CountSafe(input);
+
+Console.WriteLine($"Safe count: {safeCount}");
+
+int CountSafe(int[][] ints)
+{
+    return ints.Count(x => x.Aggregate(
+        new Agg(true, Direction.Unknown, null),
+        (agg, i) =>
+        {
+            var (isSafe, direction, previousValue) = agg;
+            var unSafe = new Agg(false, Direction.Unknown, i);
+            var safe = new Agg(true, direction, i);
+            
+            return !isSafe ? agg : 
+                previousValue == null ? safe :
+                previousValue - i == 0 ? unSafe :
+                Math.Abs(previousValue.Value - i) > 3 ? unSafe :
+                direction == Direction.Unknown ? new Agg(true, previousValue - i > 0 ? Direction.Decreasing : Direction.Increasing, i) :
+                previousValue - i > 0 && direction == Direction.Decreasing ? safe :
+                previousValue - i < 0 && direction == Direction.Increasing ? safe :
+                unSafe;
+        }).IsSafe);
+}
+
+enum Direction
+{
+    Increasing,
+    Decreasing,
+    Unknown
+}
+
+record Agg(bool IsSafe, Direction Direction, int? PreviousValue);
